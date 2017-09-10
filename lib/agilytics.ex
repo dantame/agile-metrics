6bb5edb1e5ec5b68a44e54d0a4b022bdfdf4f963
Dan Tame <dantame89@gmail.com>
2017-09-10 14:29:51 +0100

    Add monte carlo when trial

--- lib/agilytics.ex
+++ lib/agilytics.ex
@@ -5,7 +5,6 @@ defmodule Agilytics do
       last = List.last(issue.timings)
       Map.put(issue, :cycle_time, DateTime.diff(last, first))
     end)
-    |> IO.inspect
   end
 
   def avg_cycle_time(dataset) do
@@ -14,17 +13,15 @@ defmodule Agilytics do
     total_time = issues_with_cycle
     |> Enum.reduce(0, &(&1.cycle_time + &2))
 
-    avg_cycle = total_time / length(issues_with_cycle)
-    IO.inspect(avg_cycle)
+    total_time / length(issues_with_cycle)
   end
 
   def throughput_by_issue(dataset) do
-    throughput_data = dataset.issues
+    dataset.issues
     |> Enum.group_by(fn issue ->
       List.last(issue.timings)
       |> DateTime.to_date
     end)
-    IO.inspect(throughput_data)
   end
 
   def throughput(dataset) do
@@ -33,26 +30,43 @@ defmodule Agilytics do
       {k, length(v)}
     end)
     |> Enum.into(%{})
-    |> IO.inspect
   end
 
   def monte_carlo_how_many(dataset, num_days, num_trials) do
     daily_throughput = throughput(dataset)
     |> Enum.map(fn {_, v} -> v end)
 
+    monte_carlo_trial(&monte_carlo_how_many_trial/2, [daily_throughput, num_days], num_trials)
+  end
+
+  defp monte_carlo_how_many_trial(daily_throughput, num_days) do
+    Enum.reduce(1..num_days, fn (_, acc) ->
+      Enum.random(daily_throughput) + acc
+    end)
+  end
+
+  def monte_carlo_when(dataset, num_tickets, num_trials) do
+    daily_throughput = throughput(dataset)
+    |> Enum.map(fn {_, v} -> v end)
+
+    monte_carlo_trial(&monte_carlo_when_trial/2, [daily_throughput, num_tickets], num_trials)
+  end
+
+  defp monte_carlo_when_trial(daily_throughput, num_tickets) do
+    Stream.iterate(Enum.random(daily_throughput), fn _ -> Enum.random(daily_throughput) end)
+    |> Enum.reduce_while(%{days: 0, tickets: 0}, fn day_throughput, acc ->
+      if acc.tickets < num_tickets, do: {:cont, %{acc | days: acc.days + 1, tickets: acc.tickets + day_throughput}}, else: {:halt, acc}
+    end)
+    |> Map.get(:days)
+  end
+
+  defp monte_carlo_trial(trial, trial_args, num_trials) do
     trials = Enum.map(1..num_trials, fn _ ->
-      monte_carlo_trial(daily_throughput, num_days)
+      apply(trial, trial_args)
     end)
 
     Enum.map(1..100, fn p ->
       {p, Statistics.percentile(trials, p)}
-    end)
-    |> IO.inspect
-  end
-
-  defp monte_carlo__how_many_trial(daily_throughput, num_days) do
-    Enum.reduce(1..num_days, fn (_, acc) ->
-      Enum.random(daily_throughput) + acc
     end)
   end
 end
--- test/agilytics_test.exs
+++ test/agilytics_test.exs
@@ -7,10 +7,21 @@ defmodule AgilyticsTest do
       columns: ["Backlog", "Dev In", "Dev Out", "Review In", "Review Out", "Test In", "Test Out", "Release In", "Release Out", "Done"],
       issues: Enum.concat(Forge.issue_list(20), Forge.bug_list(5))
     }
+    IO.puts("CYCLE TIME")
     Agilytics.cycle_time(dataset)
+    |> IO.inspect
+    IO.puts("AVG CYCLE TIME")
     Agilytics.avg_cycle_time(dataset)
+    |> IO.inspect
+    IO.puts("THROUGHPUT")
     Agilytics.throughput(dataset)
+    |> IO.inspect
+    IO.puts("MONTE CARLO HOW MANY")
     Agilytics.monte_carlo_how_many(dataset, 30, 1000)
+    |> IO.inspect
+    IO.puts("MONTE CARLO WHEN")
+    Agilytics.monte_carlo_when(dataset, 44, 1000)
+    |> IO.inspect
     assert true
   end
 end
